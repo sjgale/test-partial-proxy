edee336ad67cf374ddbeee2fa6ccdde3d7066565
DESKTOP-ORFEHAA\sjgal <sam.gale@byu.net>
2018-07-30 23:09:13 -0600

    updated .env and description

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const https = require('https')
 const httpProxy = require('http-proxy')
 const fs = require('fs')
 
-const REAL_IP = 'http://123.12.12.123'
-const urlsToNotProxy = [
-	/.*\/real-route1\/.*/,
-	/.*\/real-route2\/.*/
+require('dotenv').config()
+
+const REAL_IP = process.env.REAL_IP
+const urlsNotToProxy = [
+	/.*\/ws\/.*/,
+	/.*\/bb\/.*/
 ]
 
 const proxy = httpProxy.createProxyServer()
@@ -27,11 +29,11 @@ http.createServer(redirect)
 
 
 function redirect(req, res) {
-	const shouldRedirect = !urlsToNotProxy.some(url => url.test(req.url))
+	const shouldRedirect = !urlsNotToProxy.some(url => url.test(req.url))
 	
 	if (shouldRedirect) {
 		return proxy.web(req, res, {
-			target: 'http://127.0.0.1:4200'
+			target: process.env.URL_TO_REDIRECT_TO
 		})
 	} else {
 		return proxy.web(req, res, {
